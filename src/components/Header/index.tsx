a068dd069dcfb49bf8afdae9a6af16061368f4f1
Danilo <dan.nunes0@gmail.com>
2021-09-05 00:20:07 -0300

    Add Header Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,20 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+
+import commonStyles from '../../styles/common.module.scss';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={commonStyles.container}>
+      <div
+        className={`${styles.headerContainer} ${commonStyles.articleContainer}`}
+      >
+        <Link href="/">
+          <a>
+            <img src="/spacetraveling-logo.svg" alt="logo" />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
 }
